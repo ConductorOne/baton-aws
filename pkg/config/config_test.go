1fac9f782b2eca2449c744fc994e7d5f2bd00793
Bjorn <bjorn.tipling@conductorone.com>
2025-08-18 12:40:26 -0700

    Fix test

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/conductorone/baton-aws/pkg/connector"
 	"github.com/conductorone/baton-sdk/pkg/test"
 	"github.com/conductorone/baton-sdk/pkg/ustrings"
 	"github.com/spf13/viper"
@@ -16,11 +17,26 @@ const (
 	s3ARN             = "arn:aws:s3:::my_corporate_bucket/exampleobject.png"
 )
 
+// validateConfig is run after the configuration is loaded, and should return an error if it isn't valid.
+func validateConfig(ctx context.Context, v *viper.Viper) error {
+	if v.GetBool(UseAssumeField.FieldName) {
+		err := ValidateExternalId(v.GetString(ExternalIdField.FieldName))
+		if err != nil {
+			return err
+		}
+		err = connector.IsValidRoleARN(v.GetString(RoleArnField.FieldName))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func TestConfigs(t *testing.T) {
 	ctx := context.Background()
 	test.ExerciseTestCasesFromExpressions(
 		t,
-		Configuration,
+		Config,
 		func(viper *viper.Viper) error { return validateConfig(ctx, viper) },
 		ustrings.ParseFlags,
 		[]test.TestCaseFromExpression{
